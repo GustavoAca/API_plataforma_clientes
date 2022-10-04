b7ced11f11fd71e26fcaed91747f4f6c727803b6
GustavoAcacio <galasdalas50@gmail.com>
2022-10-04 20:17:48 -0300

    estrutura remodelada

--- src/main/java/com/bemprotege/backend/controller/ImangemController.java
+++ src/main/java/com/bemprotege/backend/controller/ImangemController.java
@@ -3,6 +3,7 @@ package com.bemprotege.backend.controller;
 import com.bemprotege.backend.service.ImagensService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @RestController
@@ -11,7 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class ImangemController {
 
     private final ImagensService imagensService;
+    @PostMapping
+    public void upload(@RequestParam MultipartFile foto){
+        imagensService.salvarFoto(foto);
 
+    }
     @DeleteMapping("/{id_cliente}")
     public void delete(@PathVariable Long id_cliente) {
         imagensService.deletar(id_cliente);
--- src/main/java/com/bemprotege/backend/service/ImagensService.java
+++ src/main/java/com/bemprotege/backend/service/ImagensService.java
@@ -7,6 +7,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Optional;
 
 @Service
@@ -25,6 +34,29 @@ public class ImagensService {
 
     public void deletar(Long id){
         repository.deleteById(id);
+    }
+
+
+    @Value("${contato.disco.raiz}")
+    private String raiz;
+
+    @Value("${contato.disco.diretorio-fotos}")
+    private String diretorioFotos;
+
+    public void salvarFoto(MultipartFile foto) {
+        salvar(diretorioFotos, foto);
+    }
+
+    public void salvar(String diretorio, MultipartFile arquivo) {
+        Path diretorioPath = Paths.get("/tmp/contato-disco");
+        Path arquivoPath = diretorioPath.resolve(arquivo.getOriginalFilename());
+
+        try {
+            Files.createDirectories(diretorioPath);
+            arquivo.transferTo(arquivoPath.toFile());
+        } catch (IOException e) {
+            throw new RuntimeException("Problemas na tentativa de salvar arquivo.", e);
+        }
     }
 
 }
